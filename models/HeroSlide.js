b74484902bf76772fe14579397039fb8bc4e1d72
Sayain-Aditya <as4316664@gmail.com>
2026-06-07 22:09:09 +0530

    feat: expand heroSlide schema with heading, subtext, buttonText, and buttonLink fields

--- models/HeroSlide.js
+++ models/HeroSlide.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const heroSlideSchema = new mongoose.Schema({
-  image:  { type: String, required: true },
-  label:  { type: String, default: '' },
-  tag:    { type: String, default: '' },
-  active: { type: Boolean, default: true },
-  order:  { type: Number, default: 0 },
+  image:       { type: String, required: true },
+  label:       { type: String, default: '' },
+  tag:         { type: String, default: '' },
+  heading:     { type: String, default: '' },
+  subtext:     { type: String, default: '' },
+  buttonText:  { type: String, default: '' },
+  buttonLink:  { type: String, default: '/products' },
+  active:      { type: Boolean, default: true },
+  order:       { type: Number, default: 0 },
 }, { timestamps: true });
 
 module.exports = mongoose.model('HeroSlide', heroSlideSchema);
