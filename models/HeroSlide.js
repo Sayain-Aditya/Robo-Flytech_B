6df0bf7203f0d97387eb56c57cdbac2f88d9207a
Sayain-Aditya <as4316664@gmail.com>
2026-06-09 12:32:15 +0530

    fix the hero slide buy now button functionality

--- models/HeroSlide.js
+++ models/HeroSlide.js
@@ -4,10 +4,12 @@ const heroSlideSchema = new mongoose.Schema({
   image:       { type: String, required: true },
   label:       { type: String, default: '' },
   tag:         { type: String, default: '' },
+  badge:       { type: String, default: '' },
   heading:     { type: String, default: '' },
   subtext:     { type: String, default: '' },
   buttonText:  { type: String, default: '' },
   buttonLink:  { type: String, default: '/products' },
+  product:     { type: mongoose.Schema.Types.ObjectId, ref: 'Product', default: null },
   active:      { type: Boolean, default: true },
   order:       { type: Number, default: 0 },
 }, { timestamps: true });
